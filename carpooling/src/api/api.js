49b8692e6bdac03ebfed4b1848b19f2bfbf43344
Prince <prince@gmail.com>
2026-04-20 19:40:42 +0530

    Link

--- carpooling/src/api/api.js
+++ carpooling/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     // baseURL: "http://localhost:5000/api"
-    baseURL: ["https://safargo.onrender.com/api" , "http://localhost:5000/api"]
+    baseURL: "https://safargo.onrender.com/api"
 });
 
 export default API;
