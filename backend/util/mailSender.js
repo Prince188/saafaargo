09fc16a81116ee1497dd91ce2e785c10e14020fb
Prince <prince@gmail.com>
2026-05-11 08:44:47 +0530

    My Trip Page Created

--- backend/util/mailSender.js
+++ backend/util/mailSender.js
@@ -3,40 +3,36 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
     try {
 
-        console.log("Creating transporter...");
-
-        let transporter = nodemailer.createTransport({
+        const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
 
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS
             },
 
-            connectionTimeout: 10000, // IMPORTANT
+            family: 4, // FORCE IPV4
+
+            connectionTimeout: 10000,
             greetingTimeout: 10000,
             socketTimeout: 10000
         });
 
-        console.log("Before sendMail");
-
-        let info = await transporter.sendMail({
+        const info = await transporter.sendMail({
             from: process.env.MAIL_USER,
             to: email,
             subject: title,
             html: body
         });
 
-        console.log("MAIL SENT:", info);
+        console.log(info);
 
         return info;
 
     } catch (error) {
-
-        console.log("MAIL ERROR:", error);
-
+        console.log(error);
         throw error;
     }
 };
