3d1dfc7092941697e972fb7dfa108ff91adfe483
Prince <prince@gmail.com>
2026-05-16 09:10:23 +0530

    minor

--- carpooling/src/component/Footer.jsx
+++ carpooling/src/component/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         try {
             setLoading(true);
 
-            await axios.post("http://localhost:5000/api/newsletter/subscribe", {
+            await axios.post(`${process.env.REACT_APP_API_URL}/api/newsletter/subscribe`, {
                 email,
             });
 
