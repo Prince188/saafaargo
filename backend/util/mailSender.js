6e935cda138ad4816bd2f3e941830fd2df019b62
Prince <prince@gmail.com>
2026-05-24 17:47:56 +0530

    Change link

--- backend/util/mailSender.js
+++ backend/util/mailSender.js
@@ -1,28 +1,20 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const mailSender = async (email, title, body) => {
     try {
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            },
-        });
-
-        const info = await transporter.sendMail({
-            from: `"SafarGO" <${process.env.MAIL_USER}>`,
+        const response = await resend.emails.send({
+            from: "SafarGO <onboarding@resend.dev>",
             to: email,
             subject: title,
             html: body,
         });
 
-        console.log("MAIL SENT:", info.messageId);
+        console.log("MAIL SENT:", response);
 
-        return info;
+        return response;
 
     } catch (error) {
         console.log("MAIL ERROR:", error);
