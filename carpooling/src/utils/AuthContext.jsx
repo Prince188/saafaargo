8709bd9e8c2c331b3f238362b26d89a1e77e2860
Prince <prince@gmail.com>
2026-08-08 21:55:31 +0530

    API var

--- carpooling/src/utils/AuthContext.jsx
+++ carpooling/src/utils/AuthContext.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 
 const API_URL =
   process.env.REACT_APP_API_URL ||
-  "https://api.safargo.co.in/api";
+  "https://api2.safargo.co.in/api";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
